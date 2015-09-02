5e0569cf6f7f384687908faae9d0d67420c865c3
AguilerapJC <aguilerap.jc@gmail.com>
2015-09-02 16:14:28 -0500

    initial commit

---
+++ CMakeFiles/CMakeTessOpenCV1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CMakeTessOpenCV1.dir/main.cpp.o"
+  "CMakeTessOpenCV1.pdb"
+  "CMakeTessOpenCV1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CMakeTessOpenCV1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+Programa que busca e identifica los cuadros y el texto a partir de la cámara
